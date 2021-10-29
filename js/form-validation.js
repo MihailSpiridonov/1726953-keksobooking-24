487072830b7fa3db6196fe5acb5ae682d1a3fa63
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2021-10-30 01:06:31 +0300

    Правда или действие (часть 2)

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,24 +1,29 @@
-import {
-  form
-} from './page-status.js';
-import {
-  getAdSimilar
-} from './ad-similar.js';
+import {form} from './page-status.js';
+import {getAdSimilar} from './ad-similar.js';
 
-const formTitle = form.querySelector('.ad-form__title');
+const titleInput = form.querySelector('.ad-form__title');
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
+const priceInput = form.querySelector('.ad-form__price');
 const roomsSelect = form.querySelector('.ad-form__rooms');
 const guestsSelect = form.querySelector('.ad-form__capacity');
-const formAddress = form.querySelector('.ad-form__address');
+const addressInput = form.querySelector('.ad-form__address');
+const typeSelect = form.querySelector('.ad-form__type');
+const MIN_BUNGALOW = '0';
+const MIN_FLAT = '1000';
+const MIN_HOTEL = '3000';
+const MIN_HOUSE = '5000';
+const MIN_PALASE = '10000';
+const timeinSelect = form.querySelector('.ad-form__timein');
+const timeoutSelect = form.querySelector('.ad-form__timeout');
 
 
 //Рандомное значение для поля с адресом
-formAddress.value = getAdSimilar().offer.address;
+addressInput.value = getAdSimilar().offer.address;
 
 
-// Функция для валидации select "Количество комнат и количество мест"
-const validateSelect = function () {
+// Функция для валидации select "«Количество комнат» и «количество мест»"
+const validateSelect = () => {
   const valueRooms = roomsSelect.value;
   const valueGuests = guestsSelect.value;
 
@@ -34,32 +39,87 @@ const validateSelect = function () {
   }
 };
 
-const validateTitleLength = function () {
-  const valueLength = formTitle.value.length;
+
+// Валидация поля "Заголовок объявления"
+const validateTitleLength = () => {
+  const valueLength = titleInput.value.length;
 
   if (valueLength < MIN_TITLE_LENGTH) {
-    formTitle.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
+    titleInput.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
   } else if (valueLength > MAX_TITLE_LENGTH) {
-    formTitle.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH } симв.`);
+    titleInput.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH } симв.`);
   } else {
-    formTitle.setCustomValidity('');
+    titleInput.setCustomValidity('');
+  }
+};
+
+
+// Функция для добавления атрибутов
+const addAttributes = (nameAttribute) => {
+  priceInput.setAttribute('min', nameAttribute);
+  priceInput.setAttribute('placeholder', nameAttribute);
+};
+
+
+// Функция для валидации select "«Тип жилья» и поля «Цена за ночь»"
+const validateTypeSelect = () => {
+  const valueType = typeSelect.value;
+
+  if (valueType === 'bungalow') {
+    addAttributes(MIN_BUNGALOW);
+  } else if (valueType === 'flat') {
+    addAttributes(MIN_FLAT);
+  } else if (valueType === 'hotel') {
+    addAttributes(MIN_HOTEL);
+  } else if (valueType === 'house') {
+    addAttributes(MIN_HOUSE);
+  } else if (valueType === 'palace') {
+    addAttributes(MIN_PALASE);
+  }
+};
+
+
+// Функции для валидации select "«Количество комнат» и «количество мест»"
+const validateSelectTimeIn = () => {
+  const valueTimein = timeinSelect.value;
+  const itemsTimein = timeinSelect.children;
+
+  for (let i=0; i<itemsTimein.length; i++) {
+    if (valueTimein === itemsTimein[i].value) {
+      timeoutSelect.value = itemsTimein[i].value;
+    }
+  }
+};
+
+
+const validateSelectTimeOut = () => {
+  const valueTimeout = timeoutSelect.value;
+  const itemsTimeout = timeoutSelect.children;
+
+  for (let i=0; i<itemsTimeout.length; i++) {
+    if (valueTimeout === itemsTimeout[i].value) {
+      timeinSelect.value = itemsTimeout[i].value;
+    }
   }
 };
 
 
 // Функция валидации формы
-const setFormValidation = function () {
-  // Валидация поля "Заголовок объявления"
-  formTitle.addEventListener('input', validateTitleLength);
-
+const setFormValidation = () => {
+  // Валидация поля "Заголовок объявления" при вводе данных
+  titleInput.addEventListener('input', validateTitleLength);
 
   // Валидация полей "Количество комнат и количество мест"
-
   roomsSelect.addEventListener('change', validateSelect);
   guestsSelect.addEventListener('change', validateSelect);
+
+  // Валидации полей «Тип жилья» и поля «Цена за ночь»
+  typeSelect.addEventListener('change', validateTypeSelect);
+
+  // Синхронизация полей «Время заезда» и поля «Время выезда»
+  timeinSelect.addEventListener('change', validateSelectTimeIn);
+  timeoutSelect.addEventListener('change', validateSelectTimeOut);
 };
 
 
-export {
-  setFormValidation
-};
+export {setFormValidation};
