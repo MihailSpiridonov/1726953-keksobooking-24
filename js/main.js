3d1cefffcf410869b6dc3aed0052afbe6d0cc8f2
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2021-11-17 22:01:01 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-import {setFormValidation} from './form-validation.js';
+import {deActivePage} from './page-status.js';
 import {addMap} from './map.js';
 import {getData} from './api.js';
+import {setFormValidation} from './form-validation.js';
 
+
+//Вызов функции для перевода страницы в неактивное состояние
+deActivePage ();
+
+// Добавление карты на страницу
+addMap();
 
 // Вызов функции получения данных с сервера
 getData();
 
-
 // Вызов функции валидации формы
 setFormValidation();
-
-
-// Добавление карты на страницу
-addMap();
